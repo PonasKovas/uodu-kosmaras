6c109b18e0e461047d87dd510938f4d2a6d34c90
mykolas <25486263+PonasKovas@users.noreply.github.com>
2026-07-29 17:53:25 +0300

    add hardware serial number check

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,42 @@ compile_error!("Cannot compile for both M0 and M1 at the same time.");
 #[cfg(not(any(feature = "m0", feature = "m1")))]
 compile_error!("You must specify either the 'm0' or 'm1' feature.");
 
+fn verify_hardware_uid() {
+    let uid = embassy_stm32::uid::uid();
+
+    info!("Device UID: {=[u8]:#02x}", uid);
+
+    #[cfg(feature = "m0")]
+    {
+        // TODO: Replace with your actual M0 hex array once you read it from the console
+        const EXPECTED_M0_UID: [u8; 12] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+
+        // Uncomment once you put the real serial number above!
+        /*
+        if *uid != EXPECTED_M0_UID {
+            defmt::panic!("FATAL: M0 firmware flashed onto incorrect hardware! Aborting to prevent pin damage.");
+        }
+        */
+    }
+
+    #[cfg(feature = "m1")]
+    {
+        // TODO: Replace with your actual M1 hex array once you read it from the console
+        const EXPECTED_M1_UID: [u8; 12] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+
+        // Uncomment once you put the real serial number above!
+        /*
+        if *uid != EXPECTED_M1_UID {
+            defmt::panic!("FATAL: M1 firmware flashed onto incorrect hardware! Aborting to prevent pin damage.");
+        }
+        */
+    }
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    verify_hardware_uid();
+
     #[cfg(feature = "m0")]
     info!("Booting M0");
     #[cfg(feature = "m1")]
